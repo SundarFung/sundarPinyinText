ff7795d4457c2af34207d1a4b35d870f8f453162
SundarFung <fengxincheng@yiyahanyu.com>
2017-03-15 11:27:33 +0800

    show type

--- app/src/main/java/com/sundar/sundarpinyintext/MainActivity.java
+++ app/src/main/java/com/sundar/sundarpinyintext/MainActivity.java
@@ -2,14 +2,70 @@ package com.sundar.sundarpinyintext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    //单独汉字
+    PinyinText textPT;
+    //汉字设置字体
+    PinyinText text_ttf;
+    //单独拼音
+    PinyinText pinyinPT;
+    //拼音带字体
+    PinyinText pinyin_ttf;
+    //拼音汉字
+    PinyinText pinyinTextPT;
+    //拼音汉字带字体
+    PinyinText pinyinText_ttf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textPT = (PinyinText) findViewById(R.id.textPT);
+        text_ttf = (PinyinText) findViewById(R.id.text_ttf);
+        pinyinPT = (PinyinText) findViewById(R.id.pinyinPT);
+        pinyin_ttf = (PinyinText) findViewById(R.id.pinyin_ttf);
+        pinyinTextPT = (PinyinText) findViewById(R.id.pinyinTextPT);
+        pinyinText_ttf = (PinyinText) findViewById(R.id.pinyinText_ttf);
 
+        init();
+    }
 
+    private void init() {
+        String pinyinText="#yǒng#yuǎn# #Shēngrì# # #yǒng#yuǎn# #Shēngrì# # #yǒng#yuǎn# #Shēngrì# #";
+        String chineseText="#永#远# ____ 。#生日# ____！# #永#远# ____ 。#生日# ____！# #永#远# ____ 。#生日# ____！#";
+        String chineseNorText="生日生日生日生日";
+        String pinyinNorText="ShēngrìShēngrìShēngrì";
+        List<Pair<String, String>> pairList = new ArrayList<>();
+        String[] mChinese = chineseText.split("\\#");
+        String[] mPinyin = pinyinText.split("\\#");
+        for (int i = 0; i < mChinese.length; i++) {
+            if (i < mPinyin.length) {
+                pairList.add(Pair.create("" + mChinese[i], "" + mPinyin[i]));
+                Log.e("aaaaaa", "字：" + mChinese[i] + "拼音" + mPinyin[i]);
+            }
+        }
+        //汉字
+        textPT.setText(chineseNorText);
+        textPT.setTextColor(0xefef6688);
+        //汉字带字体
+        text_ttf.setText(chineseNorText,"mengmengda.ttf");
+        //拼音
+        pinyinPT.setText(pinyinNorText);
+        //拼音带字体
+        pinyin_ttf.setText(pinyinNorText,"mengmengda.ttf");
+        pinyin_ttf.setTextColor(0x88FF9912);
+        pinyin_ttf.setTextSize(60);
+        //普通的拼音汉字结构
+        pinyinTextPT.setPinyinText(pairList);
+        //普通的拼音汉字结构带字体
+        pinyinText_ttf.setPinyinText(pairList,"mengmengda.ttf");
+        pinyinText_ttf.setPinyinTextColor(0x889933FA);
+        pinyinText_ttf.setPinyinTextSize(50);
+        pinyinText_ttf.setVerticalSpacing(100);
     }
 }
--- sundarpinyintext/src/main/java/com/sundar/sundarpinyintext/PinyinText.java
+++ sundarpinyintext/src/main/java/com/sundar/sundarpinyintext/PinyinText.java
@@ -3,9 +3,13 @@ package com.sundar.sundarpinyintext;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -21,13 +25,13 @@ import java.util.List;
  * Created by SundarFung on 2017/3/14.
  * email : SundarFung@gmail.com
  */
-public class PinyinText extends View{
+public class PinyinText extends View {
     //该控件用到的属性
     private static final int[] ATTRS = {android.R.attr.textSize, android.R.attr.textColor,
             android.R.attr.textColorHint, android.R.attr.horizontalSpacing, android.R.attr.verticalSpacing};
     /**
      * 像素单位的Size 与android:textSize对应
-     * */
+     */
     private int mTextSize;
     /**
      * 两个item之间的距离
@@ -61,7 +65,7 @@ public class PinyinText extends View{
     //是否为该text设置字体
     private boolean isHasType = false;
     //字体库路径/名
-    private String textType= "";
+    private String textType = "";
     //汉字的颜色 等同于android:textColor
     private int mTextColor;
     /**
@@ -81,6 +85,9 @@ public class PinyinText extends View{
     private int mPinyinTextColor;
     // 文本数据
     private List<PinyinCompat> mPinyinCompats = new ArrayList<PinyinCompat>();
+    // for draw plain text
+    private StaticLayout mStaticLayout;
+
     public PinyinText(Context context) {
         super(context);
         init(context, null);
@@ -114,9 +121,10 @@ public class PinyinText extends View{
 
         setTextSize(mTextSize);
     }
+
     /**
      * 初始化默认值
-     * **/
+     **/
     private void initDefault() {
         Context c = getContext();
         Resources r;
@@ -134,7 +142,7 @@ public class PinyinText extends View{
         //拼音size
         mPinyinTextSize = (int) (mTextSize * PINYIN_TEXT_SIZE_RADIO);
         // spacing
-        mVerticalSpacing   = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, dm);
+        mVerticalSpacing = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, dm);
         mHorizontalSpacing = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, dm);
         mPinyinTextSpacing = mHorizontalSpacing / 2;
 
@@ -143,8 +151,10 @@ public class PinyinText extends View{
         mPinyinTextColor = 0xff999999;
         mPaint.setStyle(Paint.Style.FILL);
     }
+
     /**
      * 设置只有拼音或者汉字的内容
+     *
      * @param text plain text to display.
      */
     public void setText(String text) {
@@ -163,15 +173,17 @@ public class PinyinText extends View{
      * **/
     public void setText(String answerPinyin, String type) {
         setText(answerPinyin);
-        isHasType=false;
-        textType="";
-        if(null!=type && !type.equals("")){
-            isHasType = true ;
+        isHasType = false;
+        textType = "";
+        if (null != type && !type.equals("")) {
+            isHasType = true;
             textType = type;
         }
     }
+
     /**
      * 设置单独文字的size 拼音是汉字的0.8倍
+     *
      * @param px - text size in pixels
      */
     public void setTextSize(int px) {
@@ -181,8 +193,10 @@ public class PinyinText extends View{
         mTextSize = px;
         setPinyinTextSize((int) (px * PINYIN_TEXT_SIZE_RADIO));
     }
+
     /**
      * 设置拼音的size,如果没有设置的话会调用getTextSize()去获取
+     *
      * @param px - pinyin text size in pixels
      */
     public void setPinyinTextSize(int px) {
@@ -196,6 +210,7 @@ public class PinyinText extends View{
         requestLayout();
         invalidate();
     }
+
     private void calTextHeight() {
         // 计算汉字高度
         if (!TextUtils.isEmpty(mTextString)) {
@@ -215,10 +230,12 @@ public class PinyinText extends View{
             mPinyinHeight = 0;
         }
     }
+
     /**
      * 这是汉字颜色
      * 等同于android:textColor
-     *  8位16进制颜色
+     * 8位16进制颜色
+     *
      * @param color text color.
      */
     public void setTextColor(int color) {
@@ -230,33 +247,37 @@ public class PinyinText extends View{
      * 设置拼音颜色
      * 等同于android:textColorHint
      * 8位16进制颜色
+     *
      * @param color pinyin text color.
      */
     public void setPinyinTextColor(int color) {
         mPinyinTextColor = color;
         invalidate();
     }
+
     /**
      * 直接设置拼音和汉字之间的距离
      */
     public void setLineSpacing(int px) {
         setVerticalSpacing(px);
     }
-    /**
-     * 设置两行之间的距离
-     */
-    public void setVerticalSpacing(int px) {
-        mVerticalSpacing = px;
-        //汉字与拼音的距离是两行距离的一半
-        mPinyinTextSpacing = mVerticalSpacing / 2;
-        requestLayout();
-        invalidate();
-    }
+
     /**
      * 设置两个item之间的距离
      */
     public void setHorizontalSpacing(int px) {
         mHorizontalSpacing = px;
+        //汉字与拼音的距离是两行距离的一半
+        mPinyinTextSpacing = mHorizontalSpacing / 2;
+        requestLayout();
+        invalidate();
+    }
+
+    /**
+     * 设置两行之间的距离
+     */
+    public void setVerticalSpacing(int px) {
+        mVerticalSpacing = px;
         requestLayout();
         invalidate();
     }
@@ -302,6 +323,7 @@ public class PinyinText extends View{
         requestLayout();
         invalidate();
     }
+
     private void clearAll() {
         mPinyinCompats.clear(); // clear
 
@@ -311,18 +333,228 @@ public class PinyinText extends View{
         mTextHeight = 0;
         mPinyinHeight = 0;
     }
+
     /***
      * 设置上拼音下汉字的结构内容 同时指定拼音的字体
      * */
     public void setPinyinText(List<Pair<String, String>> answer, String pinyinType) {
         setPinyinText(answer);
-        isHasType=false;
-        textType="";
-        if(null!=pinyinType && !pinyinType.equals("")){
-            isHasType = true ;
+        isHasType = false;
+        textType = "";
+        if (null != pinyinType && !pinyinType.equals("")) {
+            isHasType = true;
             textType = pinyinType;
         }
     }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        clearAll();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mDrawType == TYPE_PINYIN_AND_TEXT && !mPinyinCompats.isEmpty()) {
+            //拼音和汉字结构
+            measurePinyinText(widthMeasureSpec, heightMeasureSpec);
+        } else if (mDrawType == TYPE_PLAIN_TEXT && !TextUtils.isEmpty(mTextString)) {
+            //普通结构
+            measurePlainText(widthMeasureSpec, heightMeasureSpec);
+        } else {
+            measureDefault(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (this.isInEditMode()) {
+            return;
+        }
+
+        if (mDrawType == TYPE_PINYIN_AND_TEXT) {
+            drawPinyinAndText(canvas);
+        } else if (mDrawType == TYPE_PLAIN_TEXT) {
+            drawPlainText(canvas);
+        }
+    }
+
+    private void measurePinyinText(int widthMeasureSpec, int heightMeasureSpec) {
+        int paddingLeft = this.getPaddingLeft();
+        int paddingRight = this.getPaddingRight();
+        int paddingTop = this.getPaddingTop();
+        int paddingBottom = this.getPaddingBottom();
+
+        // max allowed width or height
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight;
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom;
+
+        // mode
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        // measured width and height
+        int measuredWidth = modeWidth == MeasureSpec.EXACTLY ? sizeWidth : 0;
+        int measuredHeight = modeHeight == MeasureSpec.EXACTLY ? sizeHeight : 0;
+
+        int line = 0;
+        int col = 0;
+        int lineLength = 0;
+        int baseLine = 0; // top of pinyin
+        boolean newLine = false;
+
+        for (PinyinCompat compat : mPinyinCompats) {
+            int textWidth = getTextWidth(compat.text, mTextSize);
+            int pinyinWidth = getTextWidth(compat.pinyin, mPinyinTextSize);
+
+            int maxWidth = Math.max(textWidth, pinyinWidth);
+
+            if (newLine) {
+                line++;
+                col = 0;
+                newLine = false;
+            }
+
+            if (lineLength + maxWidth + (col == 0 ? 0 : mHorizontalSpacing) > sizeWidth) { // new row
+                lineLength = maxWidth;
+
+                baseLine += mTextHeight + mPinyinHeight + mPinyinTextSpacing + mVerticalSpacing;
+
+                if (modeWidth != MeasureSpec.EXACTLY) {
+                    measuredWidth = sizeWidth;
+                }
+
+                newLine = true;
+            } else {
+                if (col != 0 || line != 0) { // not the first item of first row
+                    lineLength += mHorizontalSpacing;
+                }
+                lineLength += maxWidth;
+
+                if (modeWidth != MeasureSpec.EXACTLY && measuredWidth < lineLength) {
+                    measuredWidth = lineLength;
+                    if (measuredWidth > sizeWidth) {
+                        measuredWidth = sizeWidth;
+                    }
+                }
+                col++;
+            }
+
+            compat.pinyinRect.left = lineLength - maxWidth;
+            compat.pinyinRect.right = compat.pinyinRect.left + pinyinWidth;
+            compat.pinyinRect.top = baseLine;
+            compat.pinyinRect.bottom = compat.pinyinRect.top + mPinyinHeight;
+
+            compat.textRect.left = lineLength - maxWidth;
+            compat.textRect.right = compat.textRect.left + textWidth;
+            compat.textRect.top = compat.pinyinRect.bottom + mPinyinTextSpacing;
+            compat.textRect.bottom = compat.textRect.top + mTextHeight;
+        }
+
+        if (modeHeight != MeasureSpec.EXACTLY) {
+            measuredHeight = baseLine + mPinyinHeight + mPinyinTextSpacing + mTextHeight + mTextHeight / 4;
+            if (measuredHeight > sizeHeight) {
+                measuredHeight = sizeHeight;
+            }
+        }
+        // +10来避免拼音显示不全的问题
+        setMeasuredDimension(measuredWidth + paddingLeft + paddingRight + 10, measuredHeight + paddingTop + paddingBottom + 4);
+    }
+
+    private void measurePlainText(int widthMeasureSpec, int heightMeasureSpec) {
+        int paddingLeft = this.getPaddingLeft();
+        int paddingRight = this.getPaddingRight();
+        int paddingTop = this.getPaddingTop();
+        int paddingBottom = this.getPaddingBottom();
+
+        // max allowed width or height
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec) - paddingLeft - paddingRight;
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec) - paddingTop - paddingBottom;
+
+        // mode
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        // calculate text width and height
+        mPaint.setTextSize(mTextSize);
+        mStaticLayout = new StaticLayout(mTextString, mPaint, sizeWidth, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0, false);
+
+        // measured width and height
+        int measuredWidth =
+                modeWidth == MeasureSpec.EXACTLY ? sizeWidth : Math.min(sizeWidth,
+                        (int) Math.ceil(Layout.getDesiredWidth(mTextString, mPaint)));
+        int measuredHeight = modeHeight == MeasureSpec.EXACTLY ? sizeHeight : mStaticLayout.getHeight();
+
+        setMeasuredDimension(measuredWidth + paddingLeft + paddingRight + 10, measuredHeight + paddingTop + paddingBottom + 4);
+    }
+
+    private void measureDefault(int widthMeasureSpec, int heightMeasureSpec) {
+        // max allowed width or height
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        // mode
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        // measured width and height
+        int measuredWidth = modeWidth == MeasureSpec.EXACTLY ? sizeWidth : getPaddingLeft() + getPaddingRight();
+        int measuredHeight = modeHeight == MeasureSpec.EXACTLY ? sizeHeight : getPaddingTop() + getPaddingBottom();
+
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+
+
+    private void drawPinyinAndText(Canvas canvas) {
+        int paddingLeft = this.getPaddingLeft();
+        int paddingTop = this.getPaddingTop();
+
+        for (int i = 0; i < mPinyinCompats.size(); i++) {
+            PinyinCompat compat = mPinyinCompats.get(i);
+
+            // draw text
+            mPaint.setColor(mTextColor);
+            mPaint.setTextSize(mTextSize);
+            compat.textRect.offset(paddingLeft, paddingTop);
+            canvas.drawText(compat.text, compat.textRect.left , compat.textRect.bottom, mPaint);
+
+            // draw pinyin
+            mPaint.setColor(mPinyinTextColor);
+            mPaint.setTextSize(mPinyinTextSize);
+            compat.pinyinRect.offset(paddingLeft, paddingTop);
+            //设置成一呀汉语的拼音字体
+            if(isHasType){
+                Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), textType);
+                mPaint.setTypeface(typeface);
+            }
+            canvas.drawText(compat.pinyin, compat.pinyinRect.left, compat.pinyinRect.bottom, mPaint);
+        }
+    }
+
+    private void drawPlainText(Canvas canvas) {
+        if (mStaticLayout != null) {
+            int paddingLeft = this.getPaddingLeft();
+            int paddingTop = this.getPaddingTop();
+            canvas.translate(paddingLeft, paddingTop);
+
+            mPaint.setColor(mTextColor);
+            if(isHasType){
+                Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), textType);
+                mPaint.setTypeface(typeface);
+            }
+            mStaticLayout.draw(canvas);
+        }
+    }
+
+
+
+    private int getTextWidth(String text, int textSize) {
+        mPaint.setTextSize(textSize);
+        return (int) Math.ceil(Layout.getDesiredWidth(text, mPaint));
+    }
+
     static class PinyinCompat {
         String text;
         String pinyin;
